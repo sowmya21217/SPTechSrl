ac36ef43a606aefe374c1c0b0e80b6bdff73d255
sowmya <k.l.sowmya219@gmail.com>
2022-11-01 01:51:34 +0100

    Adding Header

---
+++ Frontend/src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <ul>
+          <li><img className="logo" src= "Images/logo.jpeg" alt='SP Tech srl'/></li>
+          <li><h1 className='heading'><a href="#Home">SP Tech Srl</a></h1></li> 
+          <li><a href="#About">About</a></li>
+          <li><a href="#Services">Services</a></li>
+          <li><a href="#Career">Career</a></li>
+          <li><a href="#Contacts">Contacts</a></li>
+        </ul>
+    );
+
+}
+
+export default Header; 
